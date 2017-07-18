1b08fd6f7209d0695f33141a35368c6a7a04399d
mily rivas <mrise.translations@gmail.com>
2017-07-18 14:05:26 -0400

    ejercicio solo con agregar comentario

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){                                                                                    
+
+	$("#unopen-chat").on('click', function(){
+		var abrirChat = $("#chat").val();
+		console.log(abrirChat);
+		$("#open-chat").append(`   <div  class="col-md-12 col-lg-12">
+					            				<div class="col-md-12 col-lg-12">
+					            					`+ abrirChat +`
+					            				</div>
+					            			</div>
+					                    `)
+	})
+})
